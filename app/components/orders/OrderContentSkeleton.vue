397d12dea66bddd4283b4199bfe39a2869adea45
Bartek Wieckowski <bart.it.pro@gmail.com>
2026-04-03 07:47:47 +0200

    feat: add OrderContentSkeleton component for loading state in orders

---
+++ app/components/orders/OrderContentSkeleton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="space-y-4 animate-pulse">
+    <div class="h-8 bg-gray-100 dark:bg-gray-800 rounded-lg w-48" />
+    <div class="h-5 bg-gray-100 dark:bg-gray-800 rounded w-32" />
+    <div class="mt-6 h-80 bg-gray-100 dark:bg-gray-800 rounded-xl" />
+  </div>
+</template>
